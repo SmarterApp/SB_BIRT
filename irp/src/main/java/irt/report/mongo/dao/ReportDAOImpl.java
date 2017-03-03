e65e98866bcaa630b77be08892386f3e4c1e10b5
Vkesharia <Vkesharia@DC1KESHARIAVLT2.air.org>
2017-03-03 12:12:50 -0500

    Updating licence URl and changes done to get reportid from test_results collection and updating to next one

--- irp/src/main/java/irt/report/mongo/dao/ReportDAOImpl.java
+++ irp/src/main/java/irt/report/mongo/dao/ReportDAOImpl.java
@@ -4,31 +4,32 @@
  *
  * Distributed under the AIR Open Source License, Version 1.0 See accompanying
  * file AIR-License-1_0.txt or at
- * https://bitbucket.org/sbacoss/eotds/wiki/AIR_Open_Source_License
+ * http://www.smarterapp.org/documents/American_Institutes_for_Research_Open_Source_Software_License.pdf
  *************************************************************************/
 
 package irt.report.mongo.dao;
 
 import java.util.Date;
+import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
 import irt.report.mongo.model.BrowserTestResult;
-import irt.report.mongo.model.ReportID;
 
 @Component
 public class ReportDAOImpl implements ReportDAO
 {
 
-  private static final String RESULT_COLLECTION    = "test_results";
-  private static final String REPORT_ID_COLLECTION = "reportid";
+  private static final String RESULT_COLLECTION = "test_results";
 
   @Autowired
   private MongoOperations     mongoTemplate;
@@ -41,20 +42,24 @@ public class ReportDAOImpl implements ReportDAO
 
     if (reportJsonObj != null) {
 
+      /**
+       * Setting default value for reportid to 100
+       */
       Long reportId = (long) 100;
-      ReportID reportIdFromCollection = mongoTemplate.findOne (new Query (), ReportID.class, REPORT_ID_COLLECTION);
 
-      ReportID reportIdObjToSave = new ReportID ();
+      Query query = new Query ();
 
-      if (reportIdFromCollection != null) {
-        reportId = reportIdFromCollection.getReportId () + 1;
-        reportIdObjToSave.setReportId (reportId);
-        mongoTemplate.findAndModify (new Query (Criteria.where ("reportId").is (reportIdFromCollection.getReportId ())), new Update ().update ("reportId", reportId), ReportID.class,
-            REPORT_ID_COLLECTION);
-      } else {
-        reportIdObjToSave.setReportId (reportId);
-        mongoTemplate.insert (reportIdObjToSave, REPORT_ID_COLLECTION);
+      query.with (new Sort (new Order (Direction.DESC, "reportId")));
 
+      List<BrowserTestResult> reportIds = mongoTemplate.find (query, BrowserTestResult.class, RESULT_COLLECTION);
+
+      /**
+       * Incrementing reportid by 1 by fetching data from test_results
+       * collection
+       */
+      for (BrowserTestResult dbReportId : reportIds) {
+        reportId = Long.valueOf (dbReportId.getReportId ()) + 1;
+        break;
       }
 
       reportJsonObj.put ("reportId", reportId);
