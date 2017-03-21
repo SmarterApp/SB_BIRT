693029d5a072b3fbecd55e0a75869d694f2e1f9d
Vkesharia <Vkesharia@DC1KESHARIAVLT2.air.org>
2017-03-21 11:20:49 -0400

    Fix the manual test instructions

--- irp/src/main/webapp/Scripts/SecureBrowser/test/irtspec.js
+++ irp/src/main/webapp/Scripts/SecureBrowser/test/irtspec.js
@@ -1502,7 +1502,7 @@ IRT.ApiSpecs = {
       // Get audio recorder capabilities: SEC-21, R27, F41
       "id" : "12",
       "testName" : "Manual Test: Input / Output Device selection",
-      "instruction" : "<ol><li>Click <b>Capabilities</b> to get input and output audio selection.</li><li>Select an Input Source to use as the recording device.</li><li>Click <b>Use</b> to conclude this test.</li></ol>",
+      "instruction" : "<ol><li>Click <b>Capabilities</b> to get input source options for recording audio.</li><li>Select an Input Source to use as the recording device.</li><li>Click <b>Use</b> to conclude this test.</li></ol>",
       "testApi" : "",
       "testResult" : false,
       "details" : "",
