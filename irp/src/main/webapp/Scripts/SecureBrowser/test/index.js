f94dbd92a0618c351eb8f7ec17e178e9a4dc4bcb
Vkesharia <Vkesharia@DC1KESHARIAVLT2.air.org>
2017-07-05 12:44:32 -0400

    Not running regressive test for now as iOS device is crashing

--- irp/src/main/webapp/Scripts/SecureBrowser/test/index.js
+++ irp/src/main/webapp/Scripts/SecureBrowser/test/index.js
@@ -243,7 +243,7 @@ function runIRTAutomateTest(irtSpecApiObj, irtSpecApiJsonKey, testBrowserType,
 
           var regTestObj = eval(elementKey + "regTest");
           if (regTestObj != undefined && regTestObj != null
-              && testBrowserType == certified) {
+              && testBrowserType == 'certifiedig') {
 
             var validTestArray = eval('regTestObj' + specSeparator
                 + 'validTest');
